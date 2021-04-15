75ecf67b9a7a4adebafd6e741c8d8dce6ea53e59
Michele Nasini <michele.nasini98@gmail.com>
2021-04-16 01:06:24 +0200

    get location of linkid

--- request_spacial_data.py
+++ request_spacial_data.py
@@ -6,6 +6,7 @@ Created on Thu Apr 15 22:47:31 2021
 """
 from typing import List
 import random
+import requests
 import pandas as pd
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -14,23 +15,27 @@ from IPython.display import Image, display
 from graphviz import Source
 
 df_senior = pd.read_csv('data/Senior_TIM_v1.txt', encoding= 'unicode_escape',sep="|")
-df_senior=df_senior.groupby("linkid")
 
-def pos(linkids):
-   res=requests.get("https://s.fleet.ls.hereapi.com/1/index.json?layer=ROAD_GEOM_FCn&attributes=LINK_ID&values=&"+",".join(linkids)+apiKey=poqpzod8l70ttWkLZ1N9CJjbex7lpWc3G2JVTEClgjY")
+
+def pos(linkid):
+  # print(",".join(linkids.astype(str)))
+   res=requests.get("https://s.fleet.ls.hereapi.com/1/index.json?layer=ROAD_GEOM_FCn&attributes=LINK_ID&values="+str(linkid)+"&apiKey=__M4jTOKfFKNfDLlFxF1ZtMv8YeR4XVTKx7RgQR0_oE")
+  
    json=res.json()
-   return [(json["Layers"][i]["tileXYs"][0], json["Layers"][i]["tileXYs"][1]) for i in range(len(linkids))]
+  
+   return (json["Layers"][0]["tileXYs"][0]["x"], json["Layers"][0]["tileXYs"][0]["y"])
 
-unique_ids = df_senior.linkids.unique()
+unique_ids = df_senior.linkid.unique()
 
 coords = []
-for i in range(0, len(unique_ids), 10):
-   try:
-      ids = unique_ids[i:(i+10)]
-   except:
-      ids = unique_ids[i:]   
-   coords += pos(ids)
+for i in range(len(unique_ids)):
+    try:
+        ids = unique_ids[i]   
+        coords.append(pos(ids)) 
+    except:
+        coords.append(None)
 
-x, y = *coords
+x, y = zip(*coords)
 
 new_df = pd.DataFrame({"linkid": unique_ids, "x": x, "y": y})
+new_df.to_csv("geospatial_data.csv")
